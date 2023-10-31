80c0cef67e66e30e220fe160484a1d46ea4f8359
Yunhsiu <anita8004@gmail.com>
2023-10-31 06:50:38 +0000

    Update index.ts

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,23 +1,33 @@
-import { createRouter, createWebHistory, RouterOptions, Router, RouteRecordRaw } from 'vue-router'
+import {
+  createRouter,
+  createWebHistory,
+  RouterOptions,
+  Router,
+  RouteRecordRaw,
+} from "vue-router";
 
 const routes: RouteRecordRaw[] = [
   {
-    path: '/',
-    name: 'Home',
-    component: () => import('@/views/Home.vue')
+    path: "/home",
+    name: "Home",
+    component: () => import("@/views/Home.vue"),
   },
   {
-    path: '/login',
-    name: 'Login',
-    component: () => import('@/views/Login.vue')
-  }
-]
+    path: "/login",
+    name: "Login",
+    component: () => import("@/views/Login.vue"),
+  },
+  {
+    path: "/",
+    redirect: "/login",
+  },
+];
 
 const options: RouterOptions = {
   history: createWebHistory(),
-  routes
-}
+  routes,
+};
 
-const router: Router = createRouter(options)
+const router: Router = createRouter(options);
 
-export default router
+export default router;
