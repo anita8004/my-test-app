0491e29d42cb0b022b926aa2fd392f2199eb2b37
anita <anita_li@paradise-soft.com.tw>
2023-11-01 17:27:37 +0800

    add vite-plugin-babel-macros

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import path from 'node:path'
 import vue from '@vitejs/plugin-vue'
 import Components from 'unplugin-vue-components/vite'
 import { VantResolver } from '@vant/auto-import-resolver'
+import macrosPlugin from "vite-plugin-babel-macros";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,7 +11,8 @@ export default defineConfig({
     vue(),
     Components({
       resolvers: [VantResolver()],
-    })
+    }),
+    macrosPlugin()
   ],
   resolve: {
     alias: {
